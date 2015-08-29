a7ebf6b2bdfb2192778a94c82d89761c15eb1baa
Fabien Garcia <fab0670312047@gmail.com>
2015-08-29 17:12:10 +0200

    Init project

---
+++ Gemfile
@@ -0,0 +1,7 @@
+# If you have OpenSSL installed, we recommend updating
+# the following line to use "https"
+source 'http://rubygems.org'
+
+gem 'middleman', '~> 3.4.0'
+gem 'middleman-livereload', '~> 3.4.3'
+gem "middleman-portfolio"
---
+++ config.rb
@@ -0,0 +1,107 @@
+###
+# Compass
+###
+
+# Susy grids in Compass
+# First: gem install susy
+# require 'susy'
+
+# Change Compass configuration
+# compass_config do |config|
+#   config.output_style = :compact
+# end
+
+###
+# Page options, layouts, aliases and proxies
+###
+
+# Per-page layout changes:
+#
+# With no layout
+# page "/path/to/file.html", :layout => false
+#
+# With alternative layout
+# page "/path/to/file.html", :layout => :otherlayout
+#
+# A path which all have the same layout
+# with_layout :admin do
+#   page "/admin/*"
+# end
+
+# Proxy (fake) files
+# page "/this-page-has-no-template.html", :proxy => "/template-file.html" do
+#   @which_fake_page = "Rendering a fake page with a variable"
+# end
+
+###
+# Helpers
+###
+
+# Automatic image dimensions on image_tag helper
+# activate :automatic_image_sizes
+
+# Methods defined in the helpers block are available in templates
+# helpers do
+#   def some_helper
+#     "Helping"
+#   end
+# end
+
+# Use LiveReload
+activate :livereload
+
+
+activate :portfolio do |f|
+
+  # # thumbnail width (px)
+  # f.thumbnail_width  = 200 
+
+  # # thumbnail height (px)
+  # f.thumbnail_height = 150
+
+  # f.thumbnail_class "thumbnail"
+  
+  # f.thumbnail_link_class ""
+
+  # override default portfolio template (must be located in source/)
+  f.portfolio_template=  "portfolio.html.haml"
+
+  # override default project template (also must be in source/)
+  f.project_template= "project.html.haml"
+end
+
+# Compass configuration
+set :css_dir, 'stylesheets'
+
+set :js_dir, 'javascripts'
+
+set :images_dir, 'images'
+activate :directory_indexes
+# Build-specific configuration
+configure :build do
+  ignore 'images/*.psd'
+  ignore 'stylesheets/lib/*'
+  ignore 'stylesheets/vendor/*'
+  ignore 'javascripts/lib/*'
+  ignore 'javascripts/vendor/*'
+
+  # For example, change the Compass output style for deployment
+  activate :minify_css
+
+  # Minify Javascript on build
+  activate :minify_javascript
+
+  # Enable cache buster
+  # activate :cache_buster
+
+  # Use relative URLs
+  # activate :relative_assets
+
+  # Compress PNGs after build
+  # First: gem install middleman-smusher
+  # require "middleman-smusher"
+  # activate :smusher
+
+  # Or use a different image path
+  # set :http_path, "/Content/images/"
+end
